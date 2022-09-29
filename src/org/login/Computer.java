3a7a02ab4c018cfbc833663055a8516786339b82
tharun9696 <vijayarock555@gmail.com>
2022-09-29 15:45:33 +0530

    code

---
+++ src/org/login/Computer.java
@@ -0,0 +1,8 @@
+package org.login;
+public class Computer {
+	public void computerModel() {
+
+		System.out.println("enter the computermodel");
+	}
+
+}
---
+++ src/org/login/Desktop.java
@@ -0,0 +1,18 @@
+package org.login;
+
+
+public class Desktop extends Computer {
+	private void DesktopSize() {
+		System.out.println("enter the desktopsize");
+		// TODO Auto-generated method stub
+
+	}
+	
+	
+public static void main(String[] args) {
+	Desktop a =new Desktop();
+	a.DesktopSize();
+	a.computerModel();
+	 
+
+}}
